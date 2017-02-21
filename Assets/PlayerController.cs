79a07c8442897c8ff46fa94492e24e01a468cec0
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-02-21 15:50:22 +0000

    Initial movement and gravity

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Vector3 gravityOrigin;
+	public float gravityForce;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+		rb.AddForce((gravityOrigin - transform.position).normalized * gravityForce);
+	}
+}
