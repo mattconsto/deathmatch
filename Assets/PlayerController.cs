3657c3fea6aa1bff42492975a68e267639995116
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-02-21 16:48:14 +0000

    It works!

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,12 +15,14 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 50.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 50.0f;
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
 
 		rb.AddForce((gravityOrigin - transform.position).normalized * gravityForce);
+
+		transform.rotation = Quaternion.FromToRotation(transform.up, transform.position - gravityOrigin) * transform.rotation;
 	}
 }
