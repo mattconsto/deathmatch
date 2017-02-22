19476ba8480271ec8963323228aa9d768633e74c
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-02-22 22:36:17 +0000

    Add camera rotator

---
+++ Assets/CameraRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour {
+	public Vector3 origin = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		Update();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(origin, transform.up, Time.deltaTime);
+		transform.LookAt(origin, transform.up);
+	}
+}
