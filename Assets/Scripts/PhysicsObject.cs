4ad8c6f7e579ede578306e26c5b93736fd97a7da
oliver steptoe <oliver.steppers@gmail.com>
2017-02-23 11:25:20 +0000

    Add gun prefabs 3

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour {
+
+	public Vector3 COG = new Vector3(0,0,0);
+	public float G = 9.8f;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce((COG - transform.position).normalized * G);
+		transform.rotation = Quaternion.FromToRotation(transform.up, transform.position - COG) * transform.rotation;
+	}
+
+	void OnCollisionEnter (Collision other) {
+		rb.isKinematic = true;
+	}
+
+	void OnCollisionExit (Collision other) {
+		rb.drag = 1;
+		rb.angularDrag = 4;
+	}
+}
