7a2baef61cef75a5d348ce87d7559ed1192bad8d
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-02-26 15:07:38 +0000

    Add gamepad OS detection

---
+++ Assets/Scripts/InputManagerOSDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TeamUtility.IO;
+
+public class InputManagerOSDetector : MonoBehaviour {
+	void Start () {
+		/* Load the appropriate Gamepad bindings */
+		Debug.Log("Found " + SystemInfo.operatingSystemFamily);
+
+		InputManager manager = GetComponent<InputManager>();
+		manager.playerOneDefault = "KeyboardAndMouse";
+
+		switch(SystemInfo.operatingSystemFamily) {
+			case OperatingSystemFamily.Windows:
+				manager.playerTwoDefault   = "Windows_Gamepad1";
+				manager.playerThreeDefault = "Windows_Gamepad2";
+				manager.playerFourDefault  = "Windows_Gamepad3";
+				break;
+			case OperatingSystemFamily.MacOSX:
+				manager.playerTwoDefault   = "OSX_Gamepad1";
+				manager.playerThreeDefault = "OSX_Gamepad2";
+				manager.playerFourDefault  = "OSX_Gamepad3";
+				break;
+			default:
+			case OperatingSystemFamily.Linux:
+				manager.playerTwoDefault   = "Linux_Gamepad1";
+				manager.playerThreeDefault = "Linux_Gamepad2";
+				manager.playerFourDefault  = "Linux_Gamepad3";
+				break;
+		}
+	}
+}
