71c21d4969346512918dfdce3fe871833589f3c0
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-02-26 14:10:31 +0000

    Add invisible wall prefab

--- Assets/Scripts/GizmoRenderer.cs
+++ Assets/Scripts/GizmoRenderer.cs
@@ -7,5 +7,6 @@ public class GizmoRenderer : MonoBehaviour {
 
 	public void OnDrawGizmos() {
 		if(gizmoPath != "") Gizmos.DrawIcon(transform.position, gizmoPath, true);
+		if(GetComponent<MeshCollider>() != null) Gizmos.DrawWireMesh(GetComponent<MeshCollider>().sharedMesh, transform.position, transform.rotation, transform.localScale);
 	}
 }
