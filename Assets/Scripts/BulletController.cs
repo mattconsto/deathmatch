509c54018abe505874f2eb6344e2d2bfcb0ae361
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-06 21:53:26 +0000

    Improve clipping

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -67,16 +67,20 @@ public class BulletController : MonoBehaviour {
 				print("Hit Player");
 				destroy = true;
 				float bdamage = Mathf.Max(damageMinimum, Mathf.Pow(_lifetime / lifetime, damageFalloff) + (Random.value - 0.5f) * damageSpread) * bulletDamage * (Random.value < criticalChance ? criticalMultiplier : 1);
-				col.gameObject.GetComponent<PlayerController>().OnHurt(bdamage, incindiaryTime);
-				if(col.gameObject.GetComponent<PlayerController>().health < 0) parent.transform.parent.parent.GetComponent<PlayerController>().killCount++;
+				if(col.gameObject.GetComponent<PlayerController>().health > 0) {
+					col.gameObject.GetComponent<PlayerController>().OnHurt(bdamage, incindiaryTime);
+					if(col.gameObject.GetComponent<PlayerController>().health < 0) parent.transform.parent.parent.GetComponent<PlayerController>().killCount++;
+				}
 			}
 
 			if(col.gameObject.tag == "Target") {
 				print("Hit Target");
 				destroy = true;
 				float bdamage = Mathf.Max(damageMinimum, Mathf.Pow(_lifetime / lifetime, damageFalloff) + (Random.value - 0.5f) * damageSpread) * bulletDamage * (Random.value < criticalChance ? criticalMultiplier : 1);
-				col.gameObject.GetComponent<Destructable>().OnHurt(bdamage, incindiaryTime);
-				if(col.gameObject.GetComponent<Destructable>().health < 0) parent.transform.parent.parent.GetComponent<PlayerController>().killCount++;
+				if(col.gameObject.GetComponent<Destructable>().health > 0) {
+					col.gameObject.GetComponent<Destructable>().OnHurt(bdamage, incindiaryTime);
+					if(col.gameObject.GetComponent<Destructable>().health < 0) parent.transform.parent.parent.GetComponent<PlayerController>().killCount++;
+				}
 			}
 
 			if(explosionRadius > 0) {
